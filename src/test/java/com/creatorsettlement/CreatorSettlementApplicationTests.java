b450e6a5c90af666f88463fe9eb5e3b30d1049a8
koyunseok <ysko@ulogitech.com>
2026-05-19 23:38:42 +0900

    chore: 백엔드 프로젝트 초기 셋업

---
+++ src/test/java/com/creatorsettlement/CreatorSettlementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creatorsettlement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorSettlementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
