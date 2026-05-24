3f5510325897997103d4a312a464c85f91b1435d
koyunseok <ysko@ulogitech.com>
2026-05-24 22:50:41 +0900

    refactor: 클래스 재배치에 따른 미사용 클래스 제거

--- src/main/java/com/creatorsettlement/domain/repository/sales/dto/CancellationView.java
+++
@@ -1,6 +0,0 @@
-package com.creatorsettlement.domain.repository.sales.dto;
-
-import com.creatorsettlement.domain.model.sales.CancellationRecord;
-import com.creatorsettlement.domain.model.vo.CreatorId;
-
-public record CancellationView(CancellationRecord record, CreatorId creatorId) {}
